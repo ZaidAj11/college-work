8e43bfb4959c98e81782a1190d8dbd7cf6621bf5
Zaid Aljarrah <ZAID.ALJARRAH.2020@MUMAIL.IE>
2021-11-09 14:50:14 +0000

    Updated Assignment-03

---
+++ Assignment-03/Assignment-03/Caretaker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Caretaker
+{
+    // This will just be driver code for the Memento class 
+}
+
---
+++ Assignment-03/Assignment-03/Memento.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+class Memento : Shape
+{
+    private readonly Canvas savedCanvasState;
+
+    private Memento(Canvas stateToSave)
+    {
+        savedCanvasState = stateToSave;
+    }
+
+    public class Originator
+    {
+        private Canvas state;
+        public void Set(Canvas state) // Setting the canvas state locally
+        {
+            this.state = state;
+        }
+
+        public Memento SaveToMemento() // Saving canvas to memento 
+        {
+            return new Memento(state);
+        }
+
+        public void RestoreFromMemento(Memento memento) // Restoring version of canvas from memento
+        {
+            state = memento.savedCanvasState;
+        }
+    }
+
+     
+}
+
